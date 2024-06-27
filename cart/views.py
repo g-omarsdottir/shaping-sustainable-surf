7cf37ae1685a7b807b3eaa22b098a7797cbad2b2
Gudrun Omarsdottir <g.omarsdottir@gmail.com>
2024-06-27 16:43:34 +0000

    Add logic to app cart to add items in cart
    - Add add_to_cart view
    - Fix style and remove unused elements in cart.html

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
+
 
 def view_cart(request):
     """
@@ -8,3 +11,24 @@ def view_cart(request):
     """
 
     return render(request, "cart/cart.html")
+
+
+def add_to_cart(request, item_id):
+    """
+    Add a product to the shopping cart
+    """
+
+    product = get_object_or_404(Product, pk=item_id)
+    redirect_url = request.POST.get("redirect_url")
+
+    cart = request.session.get("cart", {})
+
+    if item_id in cart:
+        messages.info(request, f"{product.name} is already in your cart")
+    else:
+        cart[item_id] = 1
+        messages.success(request, f"Added {product.name} to your cart")
+
+    request.session["cart"] = cart
+    print(request.session["cart"])
+    return redirect(redirect_url)
