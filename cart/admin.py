9f81416bc5cdbe10574c0a5ce29eb3bbb4dfbd70
Gudrun Omarsdottir <g.omarsdottir@gmail.com>
2024-07-02 07:52:45 +0000

    Complete moving discount code model to app cart
    - Register discount code mode in admin in app cart
    - Add asterisk to blank label in country field of checkout / Order model
    - Fix margin-top style on amounts on cart.html

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import DiscountCode
+
 
 # Register your models here.
+@admin.register(DiscountCode)
+class DiscountCodeAdmin(admin.ModelAdmin):
+    """
+    Fields to display, search and manage discount codes in admin panel.
+    """
+
+    list_display = ("code", "amount", "active")
+    list_filter = ("active",)
+    search_fields = ("code", "code")
+    ordering = ("-active", "code")
+    actions = ["activate_codes", "deactivate_codes"]
+
+    def activate_codes(self, request, queryset):
+        queryset.update(active="Yes")
+    activate_codes.short_description = "Mark selected codes as active"
+
+    def deactivate_codes(self, request, queryset):
+        queryset.update(active="No")
+    deactivate_codes.short_description = "Mark selected codes as inactive"
+
--- checkout/admin.py
+++ checkout/admin.py
@@ -1,29 +1,8 @@
 from django.contrib import admin
-from .models import DiscountCode, Order, OrderItem
+from .models import Order, OrderItem
 
 
 # Register your models here.
-@admin.register(DiscountCode)
-class DiscountCodeAdmin(admin.ModelAdmin):
-    """
-    Fields to display, search and manage discount codes in admin panel.
-    """
-
-    list_display = ("code", "amount", "active")
-    list_filter = ("active",)
-    search_fields = ("code", "code")
-    ordering = ("-active", "code")
-    actions = ["activate_codes", "deactivate_codes"]
-
-    def activate_codes(self, request, queryset):
-        queryset.update(active="Yes")
-    activate_codes.short_description = "Mark selected codes as active"
-
-    def deactivate_codes(self, request, queryset):
-        queryset.update(active="No")
-    deactivate_codes.short_description = "Mark selected codes as inactive"
-
-
 class OrderItemAdminInline(admin.TabularInline):
     """
     Fields to display order items inline in OrderAdmin interface.
