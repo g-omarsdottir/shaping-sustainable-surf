f5771f0a61e00538468e2017e719e6a980d6e70c
Gudrun Omarsdottir <g.omarsdottir@gmail.com>
2024-07-10 19:47:01 +0000

    Debug: email settings

--- shaping_surf/settings.py
+++ shaping_surf/settings.py
@@ -233,6 +233,7 @@ if DEBUG is True:
 else:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
     EMAIL_HOST = "smtp.gmail.com"
     EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
     EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASS", "")
