8646bfa9beb330eb06acafe384be2389b06ea16c
Gudrun Omarsdottir <g.omarsdottir@gmail.com>
2024-07-10 13:28:19 +0000

    Fix appearance in admin interface: products / admin.py
    - Add function to display a human-readable preview of description field of products / product model in the admin interface

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import strip_tags
 from django_summernote.admin import SummernoteModelAdmin
 
 from .models import Product, Category, Subcategory
@@ -18,7 +19,7 @@ class ProductAdmin(SummernoteModelAdmin):
         "category",
         "subcategory",
         "price",
-        "description",
+        "short_description",
         "image",
         "video_url",
         "status",
@@ -32,6 +33,16 @@ class ProductAdmin(SummernoteModelAdmin):
         "name",
         "description",
     )
+
+    def short_description(self, obj):
+        """
+        Return a shortened version of the description.
+        """
+        # Strip HTML tags and truncate to 50 characters
+        clean_description = strip_tags(obj.description)
+        return (clean_description[:47] + "...") if len(clean_description) > 50 else clean_description
+
+    short_description.short_description = "Description"
 
 
 admin.site.register(Category)
