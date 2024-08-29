d7f62e3763016dec09daf9a2e70c55084e42e372
Gudrun Omarsdottir <g.omarsdottir@gmail.com>
2024-08-29 14:55:56 +0000

    Add front-end functionality for newsletter subscription
    - Add html templates for subscription and subscription success
    - Add discount code to subscribe view to pass on to success template
    - Fix redundant aria-labels on links in footer and use more descriptive words in links (instead of link “HERE” use descriptive words “privacy policy” or “contact form”)
    - Add link to newsletter subscription in footer

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 
 from .models import Subscriber
 
+@admin.register(Subscriber)
+class Subscriber(admin.ModelAdmin):
 
-admin.site.register(Subscriber)
+    fields = (
+        "name",
+        "email",
+        "unsubscribe_token",
+    )
+
+    list_display = (
+            "name",
+            "email",
+        )
+
+    search_fields = (
+        "name",
+        "email",
+    )
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,9 +1,11 @@
 import secrets
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
+from django.conf import settings
 
 from .models import Subscriber
 from .forms import SubscriberForm
+from cart.models import DiscountCode
 
 
 def subscribe(request):
@@ -12,6 +14,8 @@ def subscribe(request):
     Unsubscribe token is generated
     in save method of :model:`newsletter.Subscriber`.
     """
+    company_email = settings.DEFAULT_FROM_EMAIL
+
     if request.method == "POST":
         form = SubscriberForm(request.POST)
         if form.is_valid():
@@ -24,16 +28,33 @@ def subscribe(request):
                 messages.success(
                     request, "You have been subscribed to the newsletter!"
                 )
+                # Get newsletter discount code
+                newsletter_code = DiscountCode.objects.filter(
+                    code="ALOHA&WELCOME", active="Yes"
+                ).first()
+                context = {
+                        "newsletter_code": newsletter_code,
+                        "company_email": company_email,
+                    }
+                if not newsletter_code:
+                    messages.warning(
+                        request,
+                        f"Oops! Something went wrong "
+                        f"while getting your discount code. "
+                        f"Please contact us and we will make it up to you!"
+                    )
+                template = "newsletter/subscribe_success.html"
+                return render(request, template, context)
             else:
                 messages.info(request, "You are already subscribed!")
-
         else:
-            messages.error(request, "Please correct the errors below.")
+            messages.error(request, "Please check if your email is correct.")
     else:
         form = SubscriberForm()
 
     context = {
         "form": form,
+        "company_email": company_email,
     }
     template = "newsletter/subscribe.html"
 
