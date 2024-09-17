049c79ce97f8d96889a8abc3d071bf2499a2ed0b
Gudrun Omarsdottir <g.omarsdottir@gmail.com>
2024-09-17 18:16:20 +0000

    Debug: change postal code to shipping details

--- staticfiles/checkout/js/stripe_elements.js
+++ staticfiles/checkout/js/stripe_elements.js
@@ -76,13 +76,24 @@ document.addEventListener('DOMContentLoaded', function() {
                         address: {
                             line1: $.trim(form.street_address1.value),
                             line2: $.trim(form.street_address2.value),
-                            postal_code: $.trim(form.postcode.value),
                             city: $.trim(form.town_or_city.value),
                             state: $.trim(form.county.value),
                             country: $.trim(form.country.value),
                         }
                     }
-                }
+                },
+                shipping: {
+                    name: $.trim(form.full_name.value),
+                    phone: $.trim(form.phone_number.value),
+                    address: {
+                        line1: $.trim(form.street_address1.value),
+                        line2: $.trim(form.street_address2.value),
+                        city: $.trim(form.town_or_city.value),
+                        country: $.trim(form.country.value),
+                        postal_code: $.trim(form.postcode.value),
+                        state: $.trim(form.county.value),
+                    }
+                },
             }).then(function(result) {
                 if (result.error) {
                     var errorDiv = document.getElementById('card-errors');
